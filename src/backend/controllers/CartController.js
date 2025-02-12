46d33148729a333b12b90968287f4c68d739bdd4
billmark0093 <billmark0093@gmail.com>
2025-02-12 16:56:50 -0500

    Category, Order controllers are checking.

---
+++ src/backend/controllers/CartController.js
@@ -0,0 +1,48 @@
+import { Response } from "miragejs";
+import { formatDate, requiresAuth } from "../utils/authUtils.js";
+
+
+export const updateCartItemHandler = function (schema, request) {
+  const productId = request.params.productId;
+  const userId = requiresAuth.call(this, request);
+  try {
+    if (!userId) {
+      return new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
+    }
+    
+  } 
+};
+
+export const clearCartHandler = function (schema, request) {
+  const userId = requiresAuth.call(this, request);
+  try {
+    if (!userId) {
+      new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
+    }
+
+    this.db.users.update({ _id: userId }, { cart: [] });
+    const userCart = schema.users.findBy({ _id: userId }).cart;
+
+    return new Response(201, {}, { cart: userCart });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
