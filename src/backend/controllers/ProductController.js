90301a8884d9f17eb22f91420bf5caa0bce411a1
billmark0093 <billmark0093@gmail.com>
2025-02-13 19:03:10 -0500

    Product kevin is added.

---
+++ src/backend/controllers/ProductController.js
@@ -0,0 +1,22 @@
+import { Response } from "miragejs";
+
+
+/**
+ * This handler handles gets all products in the db.
+ * */
+
+export const getProductHandler = function (schema, request) {
+  const productId = request.params.productId;
+  try {
+    const product = schema.products.findBy({ _id: productId });
+    return new Response(200, {}, { product });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
