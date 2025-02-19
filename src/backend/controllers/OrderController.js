973383a13d1ab178870942bb2f2279754b487f87
billmark0093 <billmark0093@gmail.com>
2025-02-19 11:05:16 -0500

    Checking the current branch, test bug/fixing

--- src/backend/controllers/OrderController.js
+++ src/backend/controllers/OrderController.js
@@ -15,40 +15,13 @@ import { formatDate, requiresAuth } from "../utils/authUtils.js";
 
 export const getOrderItemsHandler = function (schema, request) {
   const userId = requiresAuth.call(this, request);
-  if (!userId) {
-    new Response(
-      404,
-      {},
-      { errors: ["The mail you entered is not registered"] }
-    );
-  }
   const userOrders = schema.usersfindBy({ _id: userId }).orders;
   return new Response(200, {}, { orders: userOrders });
 };
 
 export const addItemToOrdersHandler = function (schema, request) {
   const userId = requiresAuth.call(this, request);
-  try {
-    if (!userId) {
-      new Response(
-        404,
-        {},
-        {
-          errors: ["The email you entered is not Registered. Not Found error"],
-        }
-      );
-    }
-    const userOrders = schema.users.findBy({ _id: userId }).orders;
-    const order = JSON.parse(request.requestBody);
-    userOrders.push({
-      ...order,
-      createdAt: formatDate(),
-      updatedAt: formatDate(),
-      _id: uuid(),
-    });
-    this.db.users.update({ _id: userId }, { orders: userOrders });
-    return new Response(201, {}, { orders: userOrders });
-  } catch (error) {
+   catch (error) {
     return new Response(
       500,
       {},
