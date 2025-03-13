f9716187c309d82efd0a1ed42647de955cfd0036
baymaxfpo <billmark0093@gmail.com>
2025-03-12 22:39:33 -0400

    Diverged fixing module bugs.

--- server/index.js
+++ server/index.js
@@ -16,9 +16,10 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-}), logger();
+}), logger.createLogger({});
 
 const server = app.listen(PORT, () => {
+  logger.createLogger({});
   console.log(`Server running`);
 });
 
